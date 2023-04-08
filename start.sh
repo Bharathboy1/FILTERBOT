13ed16285f7de6ca99c25d6d64da962b5a5da4dc
Bharath s <84450562+Bharathboy@users.noreply.github.com>
2023-04-08 13:56:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,35 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Bharathboy/url-auto-delete-shortener-bot /Eva
+  git clone https://github.com/Bharathboy/url-auto-delete-shortener.git /url-auto-delete-shortener
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Eva
+  git clone $UPSTREAM_REPO /url-auto-delete-shortener
 fi
-cd /Eva
+cd /url-auto-delete-shortener
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
+##if [ -z $UPSTREAM_REPO ]
+
+#then
+
+  #echo "Cloning main Repository"
+#
+  #git clone https://github.com/LazyDeveloperr/LazyPrincess.git /LazyPrincess
+
+#else
+
+  #echo "Cloning Custom Repo from $UPSTREAM_REPO "
+
+  #git clone $UPSTREAM_REPO /LazyPrincess
+
+#fi
+
+#cd /LazyPrincess
+
+#pip3 install -U -r requirements.txt
+
+#echo "Starting Bot...."
+
+#python3 bot.py
