6a8108d43fa6a4480f503259022564f89fba43d4
Bharath s <84450562+Bharathboy@users.noreply.github.com>
2023-04-09 08:00:57 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -257,7 +257,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='https://youtube.com/@GreyMattersYT') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('JOIN', url='https://t.me/New_movies_hindi_eng') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
