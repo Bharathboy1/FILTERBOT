0760bd6c71d8fba6606da33b4af8c779f10b67de
Bharath s <84450562+Bharathboy@users.noreply.github.com>
2023-04-10 15:54:34 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -1,23 +1,14 @@
-import asyncio
 from pyrogram import Client, filters
-
 from info import CHANNELS
 from database.ia_filterdb import save_file
 
 media_filter = filters.document | filters.video | filters.audio
 
-# Create a dictionary to store the hash values and sizes of files
+# Create a dictionary to store the hash values of files
 hash_dict = {}
 
-app = Client("my_account")
-
-async def idle():
-    """Keep the program running"""
-    while True:
-        await asyncio.sleep(60)
-
-@app.on_message(filters.chat(CHANNELS) & media_filter)
-async def media_handler(bot, message):
+@Client.on_message(filters.chat(CHANNELS) & media_filter)
+async def media(bot, message):
     """Media Handler"""
     for file_type in ("document", "video", "audio"):
         media = getattr(message, file_type, None)
@@ -26,62 +17,16 @@ async def media_handler(bot, message):
     else:
         return
     
-    # Calculate the hash value and size of the file
+    # Calculate the hash value of the file
     file_hash = hash(media.file_id)
-    file_size = media.file_size
     
-    # Check if the hash value and size already exist in the dictionary
-    if (file_hash, file_size) in hash_dict:
-        # If the hash value and size exist, compare the file names
-        existing_file_name = hash_dict[(file_hash, file_size)]
-        current_file_name = media.file_name or media.title
-        if current_file_name == existing_file_name:
-            # If the file names match, delete the duplicate file
-            await bot.delete_messages(chat_id=message.chat.id, message_ids=[message.message_id])
-        else:
-            # If the file names do not match, add the file name to the dictionary
-            hash_dict[(file_hash, file_size)] = current_file_name
-            media.file_type = file_type
-            media.caption = message.caption
-            await save_file(media)
+    # Check if the hash value already exists in the dictionary
+    if file_hash in hash_dict:
+        # If the hash value exists, delete the duplicate file
+        await bot.delete_messages(chat_id=message.chat.id, message_ids=message.message_id)
     else:
-        # If the hash value and size do not exist, save the file and add the hash value and size to the dictionary
-        hash_dict[(file_hash, file_size)] = media.file_name or media.title
+        # If the hash value does not exist, save the file and add the hash value to the dictionary
+        hash_dict[file_hash] = True
         media.file_type = file_type
         media.caption = message.caption
         await save_file(media)
-
-    # Get all the messages in the channel to check for duplicates
-    channel_messages = await bot.get_messages(chat_id=message.chat.id)
-    for channel_message in channel_messages:
-        if channel_message.message_id == message.message_id:
-            # Stop iterating once the current message is reached
-            break
-        
-        for file_type in ("document", "video", "audio"):
-            channel_media = getattr(channel_message, file_type, None)
-            if channel_media is not None:
-                # Calculate the hash value and size of the file in the channel
-                channel_file_hash = hash(channel_media.file_id)
-                channel_file_size = channel_media.file_size
-                
-                if (channel_file_hash, channel_file_size) in hash_dict:
-                    # If the hash value and size exist, compare the file names
-                    existing_file_name = hash_dict[(channel_file_hash, channel_file_size)]
-                    current_file_name = channel_media.file_name or channel_media.title
-                    if current_file_name == existing_file_name:
-                        # If the file names match, delete the duplicate file in the channel
-                        await bot.delete_messages(chat_id=channel_message.chat.id, message_ids=[channel_message.message_id])
-                    else:
-                        # If the file names do not match, add the file name to the dictionary
-                        hash_dict[(channel_file_hash, channel_file_size)] = current_file_name
-
-async def main():
-    """Main Function"""
-    asyncio.ensure_future(idle())
-    await app.start()
-    await idle()
-
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main()) 
