37586b76d03bbcd19764a57aa1fe15053b835b0a
Bharath s <84450562+Bharathboy@users.noreply.github.com>
2023-04-10 13:28:38 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -4,26 +4,30 @@ from database.ia_filterdb import save_file
 
 media_filter = filters.document | filters.video | filters.audio
 
-
 # Create a dictionary to store the hash values of files
 hash_dict = {}
 
 @Client.on_message(filters.chat(CHANNELS) & media_filter)
 async def media(bot, message):
+    print("Received media message:", message)
     """Media Handler"""
     for file_type in ("document", "video", "audio"):
         media = getattr(message, file_type, None)
         if media is not None:
+            print("Found media of type", file_type)
             break
     else:
+        print("No supported media types found")
         return
     
     # Calculate the hash value of the file
     file_hash = hash(media.file_id)
+    print("Generated hash value:", file_hash)
     
     # Check if the hash value already exists in the dictionary
     if file_hash in hash_dict:
         # If the hash value exists, delete the duplicate file
+        print("Found duplicate file, deleting message...")
         await bot.delete_messages(chat_id=message.chat.id, message_ids=message.message_id)
     else:
         # If the hash value does not exist, save the file and add the hash value to the dictionary
@@ -31,3 +35,4 @@ async def media(bot, message):
         media.file_type = file_type
         media.caption = message.caption
         await save_file(media)
+        print("Saved media file:", media)
