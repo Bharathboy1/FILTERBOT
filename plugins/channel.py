d132defdd0f6defdc9bc37bff3dadaca89914d6d
Bharath s <84450562+Bharathboy@users.noreply.github.com>
2023-04-10 15:22:05 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -1,14 +1,5 @@
-from pyrogram import Client, filters
-from info import CHANNELS
-from database.ia_filterdb import save_file
-
-media_filter = filters.document | filters.video | filters.audio
-
-# Create a dictionary to store the hash values and sizes of files
-hash_dict = {}
-
 @Client.on_message(filters.chat(CHANNELS) & media_filter)
-async def media_handler(bot, message):
+async def media_handler(bot, message, message_ids=None):
     """Media Handler"""
     for file_type in ("document", "video", "audio"):
         media = getattr(message, file_type, None)
@@ -28,7 +19,7 @@ async def media_handler(bot, message):
         current_file_name = media.file_name or media.title
         if current_file_name == existing_file_name:
             # If the file names match, delete the duplicate file
-            await bot.delete_messages(chat_id=message.chat.id, message_ids=message.message_id)
+            await bot.delete_messages(chat_id=message.chat.id, message_ids=message_ids or message.message_id)
         else:
             # If the file names do not match, add the file name to the dictionary
             hash_dict[(file_hash, file_size)] = current_file_name
@@ -43,7 +34,7 @@ async def media_handler(bot, message):
         await save_file(media)
 
     # Get all the messages in the channel to check for duplicates
-    channel_messages = await bot.get_messages(chat_id=message.chat.id)
+    channel_messages = await bot.get_history(chat_id=message.chat.id)
     for channel_message in channel_messages:
         if channel_message.message_id == message.message_id:
             # Stop iterating once the current message is reached
@@ -62,7 +53,7 @@ async def media_handler(bot, message):
                     current_file_name = channel_media.file_name or channel_media.title
                     if current_file_name == existing_file_name:
                         # If the file names match, delete the duplicate file in the channel
-                        await bot.delete_messages(chat_id=channel_message.chat.id, message_ids=channel_message.message_id)
+                        await bot.delete_messages(chat_id=channel_message.chat.id, message_ids=message_ids or channel_message.message_id)
                     else:
                         # If the file names do not match, add the file name to the dictionary
                         hash_dict[(channel_file_hash, channel_file_size)] = current_file_name
