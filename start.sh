9d85908010c808468de11d47e5b30982c1b9a001
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-07-03 10:38:43 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Bharathboy1/FILTERBOT/tree/NEW-ONE.git /FILTERBOT/tree/NEW-ONE
+  git clone https://github.com/Bharathboy1/FILTERBOT.git /FILTERBOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /FILTERBOT/tree/NEW-ONE
-fiFILTERBOT/tree/NEW-ONE
-cd/
+  git clone $UPSTREAM_REPO /FILTERBOT
+fi 
+cd/FILTERBOT
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
