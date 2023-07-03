7348842fa124b20cb47a1bc53daf4591a9e5f70d
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-07-03 10:26:37 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,16 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Bharathboy/url-auto-delete-shortener-bot.git /url-auto-delete-shortener-bot
+  git clone https://github.com/Bharathboy1/FILTERBOT/tree/NEW-ONE.git /FILTERBOT/tree/NEW-ONE
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /url-auto-delete-shortener-bot
-fi
-cd /url-auto-delete-shortener-bot
+  git clone $UPSTREAM_REPO /FILTERBOT/tree/NEW-ONE
+fiFILTERBOT/tree/NEW-ONE
+cd/
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
+
 ##if [ -z $UPSTREAM_REPO ]
 
 #then
