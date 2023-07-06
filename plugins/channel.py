c9f28dccf0c19cba077321d06b6bff7df4231fb3
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-07-06 23:16:25 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -44,35 +44,46 @@ async def start(client, message):
 
 @Client.on_message(filters.command("sendall") & filters.user(ADMINS))
 async def x(app, msg):
-    args = msg.text.split(maxsplit=1)
-    if len(args) == 1:
-        return await msg.reply_text("Give Chat ID Also Where To Send Files")
-    args = args[1]
     try:
-        args = int(args)
-    except Exception:
-        return await msg.reply_text("Chat Id must be an integer not a string")
-    jj = await msg.reply_text("Processing...")
-    documents = col.find({})
-    last_msg = col.find_one({'_id': 'last_msg'})
-    if not last_msg:
-        col.update_one({'_id': 'last_msg'}, {'$set': {'index': 0}}, upsert=True)
-        last_msg = 0
-    else:
-        last_msg = last_msg.get('index', 0)
-    id_list = [{'id': document['_id'], 'file_name': document.get('file_name', 'N/A'), 'file_caption': document.get('caption', 'N/A'), 'file_size': document.get('file_size', 'N/A')} for document in documents]
-    await jj.edit(f"Found {len(id_list)} Files In The DB Starting To Send In Chat {args}")
-    for j, i in enumerate(id_list[last_msg:], start=last_msg):
+        args = msg.text.split(maxsplit=1)
+        if len(args) == 1:
+            return await msg.reply_text("Give Chat ID Also Where To Send Files")
+        args = args[1]
         try:
+            args = int(args)
+        except Exception:
+            return await msg.reply_text("Chat ID must be an integer, not a string")
+        
+        jj = await msg.reply_text("Processing...")
+        documents = col.find({})
+        last_msg = col.find_one({'_id': 'last_msg'})
+        
+        if not last_msg:
+            col.update_one({'_id': 'last_msg'}, {'$set': {'index': 0}}, upsert=True)
+            last_msg = 0
+        else:
+            last_msg = last_msg.get('index', 0)
+        
+        id_list = [{'id': document['_id'], 'file_name': document.get('file_name', 'N/A'), 'file_caption': document.get('caption', 'N/A'), 'file_size': document.get('file_size', 'N/A')} for document in documents]
+        
+        await jj.edit(f"Found {len(id_list)} Files In The DB Starting To Send In Chat {args}")
+        
+        for j, i in enumerate(id_list[last_msg:], start=last_msg):
             try:
-                await app.send_video(msg.chat.id, i['id'], caption=CUSTOM_FILE_CAPTION.format(file_name=i['file_name'], file_caption=i['file_caption'], file_size=i['file_size']))
+                try:
+                    await app.send_video(msg.chat.id, i['id'], caption=CUSTOM_FILE_CAPTION.format(file_name=i['file_name'], file_caption=i['file_caption'], file_size=i['file_size']))
+                except Exception as e:
+                    print(e)
+                    await app.send_document(msg.chat.id, i['id'], caption=CUSTOM_FILE_CAPTION.format(file_name=i['file_name'], file_caption=i['file_caption'], file_size=i['file_size']))
+                
+                await jj.edit(f"Found {len(id_list)} Files In The DB Starting To Send In Chat {args}\nProcessed: {j+1}")
+                col.update_one({'_id': 'last_msg'}, {'$set': {'index': j}}, upsert=True)
+                await asyncio.sleep(random.randint(1, 3))
             except Exception as e:
                 print(e)
-                await app.send_document(msg.chat.id, i['id'], caption=CUSTOM_FILE_CAPTION.format(file_name=i['file_name'], file_caption=i['file_caption'], file_size=i['file_size']))
-            await jj.edit(f"Found {len(id_list)} Files In The DB Starting To Send In Chat {args}\nProcessed: {j+1}")
-            col.update_one({'_id': 'last_msg'}, {'$set': {'index': j}}, upsert=True)
-            await asyncio.sleep(random.randint(1, 3))
-        except Exception as e:
-            print(e)
-    await jj.delete()
-    await msg.reply_text("Completed")
+        
+        await jj.delete()
+        await msg.reply_text("Completed")
+    
+    except Exception as e:
+        print(f"Error: {str(e)}")
