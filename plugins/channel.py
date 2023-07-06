39b2b08259adad68429ea445e7d8bd1dbcf93c97
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-07-06 16:50:29 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -115,16 +115,17 @@ async def x(app, msg):
        
         try:
             for j , i in enumerate(id_list[last_msg:]):
-        if  (Video().file_id=i['id']).file_type=="video":
-                await app.send_video(msg.chat.id , i['id'] , caption=CUSTOM_FILE_CAPTION.format(file_name=i['file_name'] , file_caption=i['file_caption'] , file_size=i['file_size']))
-                await jj.edit(f"Found {len(id_list)} Files In The DB Starting To Send In Chat {args}\nProcessed : {j+1}")
-                col.update_one({'_id':'last_msg'},{'$set':{'index':j}},upsert=True)
-                await asyncio.sleep(random.randint(8, 10))
-         elif (Document().file_id=i['id']).file_type=="document" :
-                await app.send_document(msg.chat.id , i['id'] , caption=CUSTOM_FILE_CAPTION.format(file_name=i['file_name'] , file_caption=i['file_caption'] , file_size=i['file_size']))
-                await jj.edit(f"Found {len(id_list)} Files In The DB Starting To Send In Chat {args}\nProcessed : {j+1}")
-                col.update_one({'_id':'last_msg'},{'$set':{'index':j}},upsert=True)
-                await asyncio.sleep(random.randint(8, 10))
+                if  (Video().file_id=i['id']).file_type=="video":
+                     await app.send_video(msg.chat.id , i['id'] , caption=CUSTOM_FILE_CAPTION.format(file_name=i['file_name'] , file_caption=i['file_caption'] , file_size=i['file_size']))
+                     await jj.edit(f"Found {len(id_list)} Files In The DB Starting To Send In Chat {args}\nProcessed : {j+1}")
+                     col.update_one({'_id':'last_msg'},{'$set':{'index':j}},upsert=True)
+                     await asyncio.sleep(random.randint(3,6))
+                elif (Document().file_id=i['id']).file_type=="document" :
+                     await app.send_document(msg.chat.id , i['id'] , caption=CUSTOM_FILE_CAPTION.format(file_name=i['file_name'] , file_caption=i['file_caption'] , file_size=i['file_size']))
+                     await jj.edit(f"Found {len(id_list)} Files In The DB Starting To Send In Chat {args}\nProcessed : {j+1}")
+                     col.update_one({'_id':'last_msg'},{'$set':{'index':j}},upsert=True)
+                     await asyncio.sleep(random.randint(3,6))
+            
             remaining_files = total_files - processed_files - skipped_files
             
             progress_text = f"Found {total_files} Files In The DB. Starting To Send In Chat {args}\n" \
