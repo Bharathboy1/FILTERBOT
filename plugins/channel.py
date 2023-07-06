ec5f4ec9da282e814c59910a095f7f3b09b3d47c
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-07-06 13:44:35 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -119,13 +119,16 @@ async def x(app, msg):
             file_size = get_size(int(i['file_size']))
             
             
-            media = await app.get_media(file_id)
-            
-            if media.document:
-                await app.send_document(msg.chat.id, file_id, caption=CUSTOM_FILE_CAPTION.format(file_name=file_name, file_caption=file_caption, file_size=file_size))
-            elif media.video:
-                await app.send_video(msg.chat.id, file_id, caption=CUSTOM_FILE_CAPTION.format(file_name=file_name, file_caption=file_caption, file_size=file_size))
-            
+            if message.video:
+    # Code for sending video
+                await app.send_video(msg.chat.id, message.video.file_id, caption=CUSTOM_FILE_CAPTION.format(file_name=file_name, file_caption=file_caption, file_size=file_size))
+            elif message.document:
+                if message.document.mime_type.split("/")[0] == "video":
+        # Code for sending video
+                    await app.send_video(msg.chat.id, message.document.file_id, caption=CUSTOM_FILE_CAPTION.format(file_name=file_name, file_caption=file_caption, file_size=file_size))
+                else:
+        # Code for sending other documents
+                    await app.send_document(msg.chat.id, message.document.file_id, caption=CUSTOM_FILE_CAPTION.format(file_name=file_name, file_caption=file_caption, file_size=file_size))
             processed_files += 1
             remaining_files = total_files - processed_files - skipped_files
             
