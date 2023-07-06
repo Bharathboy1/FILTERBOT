4f7d2ca8bf10f6952015ba31d854f38386df67df
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-07-06 17:25:25 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -114,17 +114,18 @@ async def x(app, msg):
             return
        
         try:
-            for j , i in enumerate(id_list[last_msg:]):
-                if  (Video().file_id==i['id']).file_type=="video":
-                     await app.send_video(msg.chat.id , i['id'] , caption=CUSTOM_FILE_CAPTION.format(file_name=i['file_name'] , file_caption=i['file_caption'] , file_size=i['file_size']))
-                     await jj.edit(f"Found {len(id_list)} Files In The DB Starting To Send In Chat {args}\nProcessed : {j+1}")
-                     col.update_one({'_id':'last_msg'},{'$set':{'index':j}},upsert=True)
-                     await asyncio.sleep(random.randint(3,6))
-                elif (Document().file_id==i['id']).file_type=="document" :
-                     await app.send_document(msg.chat.id , i['id'] , caption=CUSTOM_FILE_CAPTION.format(file_name=i['file_name'] , file_caption=i['file_caption'] , file_size=i['file_size']))
-                     await jj.edit(f"Found {len(id_list)} Files In The DB Starting To Send In Chat {args}\nProcessed : {j+1}")
-                     col.update_one({'_id':'last_msg'},{'$set':{'index':j}},upsert=True)
-                     await asyncio.sleep(random.randint(3,6))
+            file_id = i['id']
+            file_type = i['file_type']
+            caption = CUSTOM_FILE_CAPTION.format(file_name=i['file_name'], file_caption=i['file_caption'], file_size=i['file_size'])
+
+            if file_type == 'video':
+                await app.send_video(args, file_id, caption=caption)
+            elif file_type == 'document':
+                await app.send_document(args, file_id, caption=caption)
+
+            await jj.edit(f"Found {len(id_list)} Files In The DB Starting To Send In Chat {args}\nProcessed: {j + 1}")
+            col.update_one({'_id': 'last_msg'}, {'$set': {'index': j}}, upsert=True)
+            await asyncio.sleep(random.randint(5 , 7))
             
             remaining_files = total_files - processed_files - skipped_files
             
